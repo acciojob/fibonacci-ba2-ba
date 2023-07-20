eeb145f83986ee19b790d80eb301ba3a25fee9ff
ba2-ba <112476436+ba2-ba@users.noreply.github.com>
2023-07-20 23:56:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,14 +3,9 @@ function fibonacci(num) {
  let a = 0;
  let b = 1;
 
-	if(num==1)
-	{
-		return a;
-	}
-	if(num==2)
-	{
-		return b ;
-	}
+	if(num==1) return a;
+	
+	if(num==2) return b ;
 	
  //to store the sum
 
